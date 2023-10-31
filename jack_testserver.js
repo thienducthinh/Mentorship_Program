292cc4105fcdbbfd8174739b2bb21b606d7fdb81
thienducthinh <96965880+thienducthinh@users.noreply.github.com>
2023-10-31 11:40:57 -0700

    Thomas updated

--- jack_testserver.js
+++ jack_testserver.js
@@ -1,5 +1,108 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const fs = require('fs');
+const path = require('path');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+var db_config = {
+    host: '107.180.1.16',
+    user: 'fall2023team8',
+    password: 'fall2023team8',
+    database: 'fall2023team8'
+};
+
+var con;
+
+function handleDisconnect() {
+    con = mysql.createConnection(db_config); 
+    con.connect(function (err) {              
+        if (err) {                             
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }                                  
+    });                                    
+    con.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 
+            handleDisconnect();                        
+        } else {      
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
+
+app.use(bodyParser.json());
+
+app.get('/api/users', (req, res) => {
+    const email = req.query.email;
+    let query = 'SELECT * FROM mentorize_users';
+
+    if (email) {
+        query += ` WHERE email = '${email}'`;
+    }
+
+    con.query(query, (err, result, fields) => {
+        if (err) {
+            console.log('\n\n');
+            console.log('Error fetching users: ' + err);
+            res.status(500).send('Error fetching users');
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.post('/api/transfer', (req, res) => {
+    console.log(req.body)
+    const senderEmail = req.body.senderEmail;
+    const receiverEmail = req.body.receiverEmail;
+    const amount = parseFloat(req.body.amount);
+
+    if (isNaN(amount) || amount <= 0) {
+        res.status(400).send({ error: 'Invalid amount. Please enter a positive number.' });
+        return;
+    }
+
+    // Update the receiver's balance
+    const updateReceiverQuery = `
+        UPDATE mentorize_users
+        SET points = points + ?
+        WHERE email = ?
+    `;
+
+    con.query(updateReceiverQuery, [amount, receiverEmail], (err, result) => {
+        if (err) {
+            console.log('\n\n');
+            console.log('Error updating receiver balance: ' + err);
+            res.status(500).send({ error: 'Error updating receiver balance.' });
+            return;
+        }
+
+        // Balance updated successfully
+        res.send({ message: 'Balance updated successfully.' });
+    });
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static('public'));
+
+http.listen(8080, () => {
+    console.log('\nThe Web server is alive!!!\n' +
+        'It\'s listening on http://127.0.0.1:8080 or http://localhost:8080');
+});
+
+
 // Creating your own Web server with nodejs and conencting to mySQL database
 
+/*
 var http = require('http');     
 var fs = require('fs');         
 var url = require('url');       
@@ -78,3 +181,5 @@ else {
 server.listen(8080);
 console.log("\nThe Web server is alive!!!\n"  + 
     "It's listening on http://127.0.0.1:8080 or http://localhost:8080");
+
+    */
---
+++ public/index.js
@@ -0,0 +1,67 @@
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+
+    // Check the current type of the input field
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+    }
+}
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+    const passwordButton = document.getElementById('showPasswordButton');
+    passwordButton.addEventListener('click', function () {
+        togglePasswordVisibility();
+    });
+});
+
+$(document).ready(function () {
+    const loginForm = document.getElementById('loginForm');
+    const loginButton = document.getElementById('loginButton');
+    const errorText = document.getElementById('errorText');
+    const passwordInput = document.getElementById('password');
+
+    function login() {
+        const emailInput = document.getElementById('email').value;
+        const passwordInput = document.getElementById('password').value;
+
+        $.get('/api/users', function(users){
+            let userFound = false;
+            users.forEach(function(user){
+                if (user.email === emailInput) {
+                    userFound = true;
+                    if (user.password === passwordInput) {
+                        // Redirect to the next page if email and password match
+                        // Assuming userEmailAddress contains the email address
+                        if (user.role === 1) {
+                            window.location.href = 'mentor.html?email=' + encodeURIComponent(user.email);
+                        } else {
+                            window.location.href = 'mentee.html?email=' + encodeURIComponent(user.email);
+                        }
+                    } else {
+                        errorText.textContent = 'Incorrect password';
+                    }
+                }
+            });
+            
+            if (!userFound) {
+                errorText.textContent = 'User not found';
+            }
+        });
+            
+    }
+
+    // Event listener for the login button click
+    loginButton.addEventListener('click', login);
+
+    document.addEventListener('keydown', function(event) {
+        // Check if the pressed key is "Enter"
+        if (event.key === 'Enter' || event.key === 'NumpadEnter') {
+            // Your code to execute when Enter is pressed
+            // For example, you can call a function or perform an action here
+            // E.g., submit a form, trigger a button click, etc.
+            login();
+        }
+    });
+});
