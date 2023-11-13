9045b92e91c60fdc56491f9ee487d3327689828b
wjmellon <94713257+wjmellon@users.noreply.github.com>
2023-11-13 13:36:39 -0800

    server fix

--- server.js
+++ server.js
@@ -88,6 +88,38 @@ app.post('/api/transfer', (req, res) => {
 });
 
 
+app.post('/api/transfer2', (req, res) => {
+    console.log(req.body)
+    const senderEmail = req.body.senderEmail;
+    const receiverEmail = req.body.receiverEmail;
+    const role = parseFloat(req.body.role);
+
+    if (isNaN(role)) {
+        res.status(400).send({ error: 'Invalid amount. Please enter a positive number.' });
+        return;
+    }
+    
+    // Update the receiver's balance
+    const updateReceiverQuery = `
+        UPDATE mentorize_users
+        SET role = role + ?
+        WHERE email = ?
+    `;
+
+    con.query(updateReceiverQuery, [role, receiverEmail], (err, result) => {
+        if (err) {
+            console.log('\n\n');
+            console.log('Error updating role: ' + err);
+            res.status(500).send({ error: 'Error updating role.' });
+            return;
+        }
+
+        // Balance updated successfully
+        res.send({ message: 'role updated successfully.' });
+    });
+});
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
